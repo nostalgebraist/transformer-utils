851839514bc8889882b5d63da6d2755a4cb99f43
nostalgebraist <nostalgebraist@gmail.com>
2021-05-10 18:22:28 -0700

    fix

--- src/transformer_utils/low_memory/enable.py
+++ src/transformer_utils/low_memory/enable.py
@@ -6,7 +6,7 @@ from ..util.tfm_utils import huggingface_model_local_paths
 _TFM_PRETRAINED_MODEL_FROM_PRETRAINED_ORIGINAL = transformers.modeling_utils.PreTrainedModel.from_pretrained
 
 
-def low_memory_from_pretrained(cls, pretrained_model_name_or_path, *args, **kwargs):
+def low_memory_from_pretrained(pretrained_model_name_or_path, *args, **kwargs):
     config_path, model_path = huggingface_model_local_paths(pretrained_model_name_or_path)
 
     model = low_memory_load(config_path=config_path, model_path=model_path)
