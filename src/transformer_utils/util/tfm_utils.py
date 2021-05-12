dac1ce7cc790dba97689e466393ba5a3d18f0195
nostalgebraist <nostalgebraist@gmail.com>
2021-05-11 19:18:01 -0700

    fix ctrl config

--- src/transformer_utils/util/tfm_utils.py
+++ src/transformer_utils/util/tfm_utils.py
@@ -1,4 +1,26 @@
+import json
+
 import transformers.file_utils
+from transformers.models.auto.configuration_auto import CONFIG_MAPPING
+
+
+def fix_config_with_missing_model_type(model_name, config_path):
+    with open(config_path, 'r', encoding='utf-8') as f:
+        config = json.load(f)
+
+    model_type = config.get('model_type')
+
+    # cf https://github.com/huggingface/transformers/blob/v4.5.1/src/transformers/models/auto/configuration_auto.py#L403
+    #
+    # we reproduce that logic here, but save the fixed config to the json file
+    # so it will work more robustly, i.e. even if you are not using `AutoConfig`
+    if model_type is None:
+        for pattern, config_class in CONFIG_MAPPING.items():
+            if pattern in model_name:
+                config['model_type'] = config_class.model_type
+
+    with open(config_path, 'w', encoding='utf-8') as f:
+        json.dump(config, f)
 
 
 def get_local_path_fom_huggingface_cdn(key, filename):
@@ -15,6 +37,8 @@ def get_local_path_fom_huggingface_cdn(key, filename):
 
 def huggingface_model_local_paths(model_name):
     config_path = get_local_path_fom_huggingface_cdn(model_name, "config.json")
+
+    fix_config_with_missing_model_type(model_name, config_path)
 
     model_path = get_local_path_fom_huggingface_cdn(model_name, "pytorch_model.bin")
 
