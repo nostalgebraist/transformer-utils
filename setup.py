3c6ee307a5a04d5ebbc67c18d9d725539f010278
nostalgebraist <nostalgebraist@gmail.com>
2021-05-14 16:09:05 -0700

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author="nostalgebraist",
     author_email="nostalgebraist@gmail.com",
     license="MIT",
-    version='0.0.8',
+    version='0.0.9',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
