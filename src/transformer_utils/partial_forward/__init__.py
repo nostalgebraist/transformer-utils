d0fcbe09e7dc1d50e9c9a237f1d3c7924693ffa0
nostalgebraist <nostalgebraist@gmail.com>
2021-05-14 14:46:15 -0700

    output name validation

--- src/transformer_utils/partial_forward/__init__.py
+++ src/transformer_utils/partial_forward/__init__.py
@@ -1,29 +1,75 @@
 import warnings
+import inspect
 
 from ..util.python_utils import make_print_if_verbose
 
 
-PARTIAL_FORWARD_FORCE_FALSE_KWARGS = [
+PARTIAL_FORWARD_FORCE_FALSE_KWARGS = {
     "use_cache",
     "output_attentions",
     "output_hidden_states",
-    "return_dict"
-]
+    "return_dict",
+}
 
-PARTIAL_FORWARD_FORCE_FALSE_KWARGS_MSG = """`partial_forward` was passed the argument {} but will ignore it.
+PARTIAL_FORWARD_FORCE_FALSE_KWARGS_MSG = """`partial_forward` was passed the argument {kwarg} but will ignore it.
 
 `partial_forward` ignores arguments that configure output shape in `transformers`, since its output shape is configured entirely through the `output_names` argument."""
+
+VALIDATE_OUTPUT_BASE_MODEL_MSG = """Some `output_names` were not found on the model (a `{model_class_name}`), but exist on its base model (a `{base_model_class_name}`).
+
+Try either passing `model.base_model` as the model, OR adding the string '{base_model_prefix}.' to the start of each output name.
+
+Names not found: {names}"""
+
+VALIDATE_OUTPUT_NOT_FOUND_MSG = """Some `output_names` were not found on the model.
+
+To see valid output names, try `dict(model.named_modules()).keys()`.
+
+Names not found: {names}"""
 
 
 class AfterStoppingPointException(Exception):
     pass
 
 
+def _validate_output_names(model, output_names):
+    if output_names is None:
+        return
+
+    findable_names = dict(model.named_modules()).keys()
+
+    findable_names_base_model = set()
+    if hasattr(model, "base_model") and hasattr(model, "base_model_prefix"):
+        findable_names_base_model = dict(model.base_model.named_modules()).keys()
+
+    problem_names = [name for name in output_names if name not in findable_names]
+
+    base_model_names = [
+        name for name in problem_names if name in findable_names_base_model
+    ]
+
+    if len(base_model_names) > 0:
+        raise ValueError(
+            VALIDATE_OUTPUT_BASE_MODEL_MSG.format(
+                model_class_name=model.__class__.__name__,
+                base_model_class_name=model.base_model.__class__.__name__,
+                base_model_prefix=model.base_model_prefix,
+                names=base_model_names,
+            )
+        )
+
+    if len(problem_names) > 0:
+        raise ValueError(VALIDATE_OUTPUT_NOT_FOUND_MSG.format(names=problem_names))
+
+
 def add_partial_forward_hooks(model, verbose=False, debug=False, output_names=None):
     vprint = make_print_if_verbose(verbose)
     dprint = make_print_if_verbose(debug)
 
+    _validate_output_names(model, output_names)
+
     can_skip = output_names is not None
+    can_skip = can_skip and hasattr(model, "_partial_forward_force_false_kwargs")
 
     names_to_mods = {}
     indices_to_names = {}
@@ -47,6 +93,11 @@ def add_partial_forward_hooks(model, verbose=False, debug=False, output_names=No
     if can_skip:
         dprint("already have partial forward hooks, skipping")
         return
+
+    sig = inspect.signature(model.__class__.forward)
+    model._partial_forward_force_false_kwargs = (
+        PARTIAL_FORWARD_FORCE_FALSE_KWARGS.intersection(sig.parameters.keys())
+    )
 
     def _record_to_sink_hook(module, input, output) -> None:
         if hasattr(model, "_output_sink_names"):
@@ -84,12 +135,14 @@ def partial_forward(
 ):
     vprint = make_print_if_verbose(verbose)
 
-    for k in PARTIAL_FORWARD_FORCE_FALSE_KWARGS:
-        if kwargs.get(k):
-            warnings.warn(PARTIAL_FORWARD_FORCE_FALSE_KWARGS_MSG.format(repr(k)))
-        kwargs[k] = False
+    add_partial_forward_hooks(
+        model, verbose=verbose, debug=debug, output_names=output_names
+    )
 
-    add_partial_forward_hooks(model, verbose=verbose, debug=debug, output_names=output_names)
+    for k in model._partial_forward_force_false_kwargs:
+        if kwargs.get(k):
+            warnings.warn(PARTIAL_FORWARD_FORCE_FALSE_KWARGS_MSG.format(kwarg=repr(k)))
+        kwargs[k] = False
 
     model._output_sink_names = output_names
 
