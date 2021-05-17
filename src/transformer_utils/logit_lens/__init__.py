14de26c9ac2e9e8e905da0794cc275a0c3b08834
nostalgebraist <nostalgebraist@gmail.com>
2021-05-17 13:01:54 -0700

    make public api easier to find

--- src/transformer_utils/logit_lens/__init__.py
+++ src/transformer_utils/logit_lens/__init__.py
@@ -1,8 +1,10 @@
 from .hooks import make_lens_hooks, clear_lens_hooks
 from . import plotting
+from .plotting import plot_logit_lens
 
 __all__ = [
     'make_lens_hooks',
     'clear_lens_hooks',
     'plotting',
+    'plot_logit_lens'
 ]
